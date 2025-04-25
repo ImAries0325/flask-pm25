eaed6f8023659868268dbad1a32111f6c86d5a83
Aries <zxc.055510@gmail.com>
2025-04-25 21:40:58 +0800

    完成資料庫的存取跟顯示表格

--- app.py
+++ app.py
@@ -2,12 +2,19 @@ from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
 import pymysql
+from pm25 import get_pm25_data_from_mysql
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    columns, datas = get_pm25_data_from_mysql()
+    return render_template("index.html", columns=columns, datas=datas)
+
+
+@app.route("/books")
+def books_page():
     # return f"<h1>Hello World!</h1><br>{datetime.now()}"
     books = [
         {
@@ -39,7 +46,7 @@ def index():
     username = "jerry"
     nowtime = datetime.now().strftime("%Y-%m-%d")
     print(username, nowtime)
-    return render_template("index.html", name=username, now=nowtime, books=books)
+    return render_template("books.html", name=username, now=nowtime, books=books)
 
 
 @app.route("/bmi")
@@ -53,17 +60,22 @@ def get_bmi():
     bmi = round(eval(weight) / (eval(height) / 100) ** 2, 2)
 
     return render_template("bmi.html", height=height, weight=weight, bmi=bmi)
+    # return render_template("bmi.html", **locals())
 
     # return {"height": height, "weight": weight, "bmi": bmi}
 
 
-@app.route("/pm25-data")
-def get_pm25_data():
-    api_url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=CSV"
-    df = pd.read_csv(api_url)
-    df["datacreationdate"] = pd.to_datetime(df["datacreationdate"])
-    df1 = df.dropna()
-    return df1.values.tolist()
+# @app.route("/pm25-data")
+# def get_pm25_data():
+#     api_url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=CSV"
+#     df = pd.read_csv(api_url)
+#     df["datacreationdate"] = pd.to_datetime(df["datacreationdate"])
+#     df1 = df.dropna()
+#     return df1.values.tolist()
 
 
 app.run(debug=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
